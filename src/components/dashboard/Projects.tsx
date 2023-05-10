302ae5011fe8ff089a33191d58eabfb2292d77ec
Andrew Lupin <andrewlupin@gmail.com>
2023-05-10 16:31:10 +0200

    Andrew Lupin Projects edit.

--- src/components/dashboard/Projects.tsx
+++ src/components/dashboard/Projects.tsx
@@ -38,21 +38,54 @@ const Blog = () => {
                           Refocus
                         </Link>
                       </Grid>
-                      <Grid item xs={12} md={3} lg={3} style={{ marginLeft: '2vw' }}>
+                      <Grid item xs={12} md={3} lg={3} style={{ marginLeft: '1vw' }}>
                         <Link href="#" className="projectTagBorder">
                           2022
                         </Link>
                       </Grid>
                     </Grid>
                     <Grid container>
-                      <Grid item xs={12} md={9} lg={9}>
-                        <Link href="#" style={{ width: '30%', fontSize: '2.083vw', textDecoration: 'none', color: 'black', fontWeight: 'bold', margin: '9vw 0 0 0.5vw' }}>
+                      <Grid item xs={12} md={9} lg={9} style={{ margin: '9vw 0 0 1.5vw' }}>
+                        <Link href="#" style={{ fontSize: '1.8vw', textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>
                           Путеводитель по IT
                         </Link>
                       </Grid>
                       <Grid item xs={12} md={3} lg={3} style={{ marginLeft: '2vw' }}>
                         <Link href="#">
-                          
+                        </Link>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} md={11} lg={11} style={{ backgroundImage: 'url(images/projects/югвидео.png)', backgroundSize: 'cover', margin: 'auto', height: '36.8vw' }}>
+                <Grid container>
+                  <Grid item xs={12} md={12} lg={12}>
+                    <Link href="#" className="projectTag">
+                      Сайт «Под ключ»
+                    </Link>
+                  </Grid>
+                  <Grid item xs={12} md={4} lg={4} style={{ backgroundColor: 'white', width: '36.77vw', height: '16.14vw', margin: '14.6vw 0 0 60vw', borderRadius: '40px' }}>
+                    <Grid container>
+                      <Grid item xs={12} md={8} lg={8}>
+                        <Link href="#" className="projectTagBorder" style={{ width: '30%' }}>
+                          Югвидео
+                        </Link>
+                      </Grid>
+                      <Grid item xs={12} md={3} lg={3} style={{ marginLeft: '1vw' }}>
+                        <Link href="#" className="projectTagBorder">
+                          2022
+                        </Link>
+                      </Grid>
+                    </Grid>
+                    <Grid container>
+                      <Grid item xs={12} md={10} lg={10} style={{ margin: '7vw 0 0 1.5vw' }}>
+                        <Link href="#" style={{ fontSize: '1.8vw', textDecoration: 'none', color: 'black', fontWeight: 'bold', lineHeight: 'normal' }}>
+                          Компания, занимающаяся системами безопасности
+                        </Link>
+                      </Grid>
+                      <Grid item xs={12} md={2} lg={2} style={{ marginLeft: '2vw' }}>
+                        <Link href="#">
                         </Link>
                       </Grid>
                     </Grid>
